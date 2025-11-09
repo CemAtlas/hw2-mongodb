9166dab41877afa79388f0522278bee5cf142d4f
Cem-Atlas <cematlas00@gmail.com>
2025-11-10 01:35:04 +0300

    HW5 Auth: users, sessions, tokens, authenticate middleware, user-scoped contacts

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,28 @@
+import createHttpError from 'http-errors';
+import { Session } from '../models/Session.js';
+import { User } from '../models/User.js';
+
+export const authenticate = async (req, _res, next) => {
+  try {
+    const auth = req.headers.authorization || '';
+    const [type, token] = auth.split(' ');
+    if (type !== 'Bearer' || !token) throw createHttpError(401, 'Not authorized');
+
+    const session = await Session.findOne({ accessToken: token });
+    if (!session) throw createHttpError(401, 'Not authorized');
+
+    const now = new Date();
+    if (now > session.accessTokenValidUntil) {
+      throw createHttpError(401, 'Access token expired');
+    }
+
+    const user = await User.findById(session.userId);
+    if (!user) throw createHttpError(401, 'Not authorized');
+
+    req.user = { _id: user._id, email: user.email, name: user.name };
+    req.session = { _id: session._id };
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
